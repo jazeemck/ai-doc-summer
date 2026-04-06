bc2ccdcf3925d26454f61711a2f48d3247d0b550
jazeemck <jazeemmuhammed53@gmail.com>
2026-04-06 06:18:23 +0530

    Architect: Restructure to 100% Vercel Serverless and remove legacy /server

--- server/test_openai.ts
+++
@@ -1,3 +0,0 @@
-import * as openai from 'openai';
-console.log('OpenAI version:', (openai.default && (openai.default as any).VERSION) || 'unknown');
-console.log('OpenAI export names:', Object.keys(openai));
--- server/test_pdf.js
+++
@@ -1,9 +0,0 @@
-const pdfParse = require('pdf-parse');
-console.log('pdfParse type:', typeof pdfParse);
-console.log('pdfParse keys:', Object.keys(pdfParse || {}));
-
-if (typeof pdfParse === 'function') {
-  console.log('pdfParse is a function!');
-} else {
-  console.log('pdfParse is NOT a function - check if you need pdfParse.default or something');
-}
